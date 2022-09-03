b93bdc2f60048ccc14b6aa792f0c9bd4de859c02
Pranjal <pranjalsingh531@gmail.com>
2022-09-03 16:58:49 +0530

    onboarding part done

--- frontend/src/Pages/LandingPage.js
+++ frontend/src/Pages/LandingPage.js
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react"
+import LandingComponent from "../Components/LandingComponent";
+import OnBoarding from "../Components/OnBoarding";
+
 const LandingPage = ()=>{
+    const [page,setPage]= useState('1');
+
+    useEffect(()=>{
+        
+        setTimeout(()=>{
+            setPage('2');
+        },1000)
+       
+    },[])
+
+    const PageChanger = (page)=>{
+        setPage(page);
+    }
+
      return(
-        <div>
-            <p>Landing Page</p>
-        </div>
+        <>
+        {(page==='1'||page==='2') && <div className="flex flex-col items-center h-screen">
+            <p style={{marginTop:"50vh"}} className="text-2xl font-semibold  inline-block">
+                <span style={{color:"#06283D"}}>Image</span>
+                <span style={{color:"#1363DF"}}>Book</span>
+            </p>
+            {page==='2' && <LandingComponent PageChanger={PageChanger}/> }
+        </div>}
+        {page==='3' && <OnBoarding/>}
+        </>
      )
 }
 export default LandingPage;
