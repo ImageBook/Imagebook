8f2229c824d587bbf84d20686f0f2064ccae92f0
Arif <iariful950@gmail.com>
2022-09-09 11:32:08 +0600

    getUser not working

--- backend/Routes/appRouter.js
+++ backend/Routes/appRouter.js
@@ -2,25 +2,30 @@ const express = require("express");
 const router = express.Router();
 const User = require("../Schemas/UserSchema");
 
-router.route('/postUsers').post((req,res)=>{
-    const user = {
-        name: req.body.name,
-        number: req.body.number,
-        accountCreationDate: Date.now()
-      };
-      const newUser = new User({
-        name: user.name,
-        number: user.number,
-        accountCreationDate:user.accountCreationDate,
-        respects: [],
+router.route('/postUsers').post((req, res) => {
+  const user = {
+    name: req.body.name,
+    number: req.body.number,
+    accountCreationDate: Date.now()
+  };
+  const newUser = new User({
+    name: user.name,
+    number: user.number,
+    accountCreationDate: user.accountCreationDate,
+    respects: [],
 
-      });
-      newUser.save();
-      res.send("user created");
+  });
+  newUser.save();
+  res.send("user created");
 })
 
-router.route('/getUsers').get((req,res)=>{
-    User.find().then((foundData)=>res.send(foundData))
+router.route('/getUsers').get((req, res) => {
+  User.find().then((foundData) => res.send(foundData))
+})
+router.route('/getUsers/:number').get((req, res) => {
+  const phone = req.params.number;
+  User.findOne({ number: phone }).then((foundData) => res.send(foundData))
+
 })
 
 module.exports = router;
