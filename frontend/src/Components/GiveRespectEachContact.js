6bf292c3636b0f3abd5c9b50bb099e65d144a5da
Arif <iariful950@gmail.com>
2022-09-19 11:43:58 +0600

    made changes

--- frontend/src/Components/GiveRespectEachContact.js
+++ frontend/src/Components/GiveRespectEachContact.js
@@ -10,7 +10,6 @@ const GiveRespectEachContact = (props) => {
       style={{ backgroundColor: "#F5F8FA" }}
       className="flex gap-2 w-11/12 mx-auto p-[12px]  rounded-xl" onClick={navigateRecordVideo}
       
-      
       // onClick={(e)=>props.setModalOpen({
       //   number: props.item.number,
       //   name: props.item.name,
