7022c78a0eac28e8d0848a892ce3e7bf57b8720b
Arif <iariful950@gmail.com>
2022-09-19 17:48:21 +0600

    heroku changed

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f1ag36g.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f1ag36g.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.st55vqv.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(uri, () => console.log('connected'));
 
 app.use('/', require('./Routes/appRouter'))
